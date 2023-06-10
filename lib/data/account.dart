83a0996028f7cd5517e4477c8b98c3675bfd70a2
StarJins <kinjn9495@naver.com>
2023-06-10 17:25:44 +0900

    firebase auth를 통한 로그인 구현

--- lib/data/account.dart
+++ lib/data/account.dart
@@ -1,19 +1,32 @@
-import 'dart:convert';
 import 'dart:async';
-import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Account {
   late final int currentId;
+  final _authentication = FirebaseAuth.instance;
+
+  // 추후 임시 idx가 아닌 firebase에서 제공하는 uid를 사용할 예정
+  final Map<String, int> tmpEmailToIdx = {
+    "test@email.com" : 1,
+    "jin@email.com" : 2,
+    "sim@email.com" : 3,
+    "kwon@email.com" : 4,
+  };
 
   Future<bool> checkIdPw(final String userId, final String userPw) async {
-    String jsonData = await rootBundle.loadString('dummy_data/user_account.json');
-    dynamic account = jsonDecode(jsonData);
+    try {
+      final account = await _authentication.signInWithEmailAndPassword(
+        email: userId,
+        password: userPw,
+      );
 
-    for (var x in account['user_account']) {
-      if (x['email'] == userId && x['password'] == userPw) {
-        this.currentId = x['id'];
+      if (account.user != null) {
+        this.currentId = tmpEmailToIdx[account.user!.email]!;
         return true;
       }
+    }
+    catch (e) {
+      print(e);
     }
 
     return false;
